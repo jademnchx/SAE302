b852f90fdfb7ffcd2b4b5a76c95bdb8bc699225e
jademnchx <jade.mnchx@gmail.com>
2022-11-25 09:44:07 +0100

    jade

--- client.py
+++ client.py
@@ -4,15 +4,19 @@ BYE = 'bye'
 EXIT = 'exit'
 msg = ''
 data = ''
-
-if __name__ == '__main__':
-    client_socket = socket.socket()
-    client_socket.connect(('127.0.0.1', 10000))
-    print ("Connected to server")
+        
+def client() :
     while msg != BYE and data != BYE :
         msg = input('Enter a message to send: ')
         client_socket.send(msg.encode())
         data = client_socket.recv(1024).decode()
         print ("Received from server: ", data)
-    client_socket.close()
-    print ("Connection closed")
+    else:
+        client_socket.close()
+        print ("Connection closed")
+
+if __name__ == '__main__':
+    client_socket = socket.socket()
+    client_socket.connect(('127.0.0.1', 10000))
+    print ("Connected to server")
+    client()
