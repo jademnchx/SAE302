d5edc89a161c6289762e1991f6503bfebaff28ec
jademnchx <jade.mnchx@gmail.com>
2022-11-30 15:58:27 +0100

    jm

--- client_test_1.py
+++ client_test_1.py
@@ -1,12 +1,14 @@
 
 from client import Client
+
 def server():
     host = 'localhost'
     port = 10000
-    heirm = server(host, port)
-    heirm.connect()
-    rep=heirm.send('connected')
+    heirm = Client(host, port)
+    heirm.connected()
+    print ("Connected to server")
+    rep=heirm.send('error __send')
     if rep == 'Socket is connected':
-        print ("Server is not connected")
+        print ("Socket is not connected")
     else :
         print(rep)
