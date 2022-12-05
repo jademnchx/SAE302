a631105abce271bc590b8e8728212b5f3c7c256d
jademnchx <jade.mnchx@gmail.com>
2022-12-05 14:31:03 +0100

    jm

--- client.py
+++ client.py
@@ -1,37 +1,51 @@
-import socket, threading
+import socket, threading, sys
 
-class Client():
+class Client(threading.Thread):
     
     host = 'localhost'
     port = 10000
     
     def __init__(self, host, port):
+        super().__init__()
         self.__host = host
         self.__port = port
-        self.__sock = None
-        
-    def is_connected(self):
-        return(self.__sock != None)
-    
-    def connected(self):
-        threading.Thread(target=self.__connected).start()
-    
-    def __connected(self):
         self.__sock = socket.socket()
-        self.__sock.connect((self.__host, self.__port))
-        self.__sock.send('connected'.encode())
-      
+    
+    def __connect(self) -> int:
+        try :
+            self.__sock.connect((self.__host,self.__port))
+        except ConnectionRefusedError:
+            print ("connection refused")
+            return -1
+        except ConnectionError:
+            print ("connection error")
+            return -1
+        else :
+            print ("connection done")
+            return 0
         
-    def send(self,msg):
-        threading.Thread(target=self.__send, args=[msg,]).start()
-    
-    def __send(self, msg):
-       # if self.is_connected():
-        self.__sock.send(msg.encode())
-        msg = self.__sock.recv (1024).decode()
-        print(msg)
-        #else:
-         #   print("error __send")
-    
-    def close(self):
+    def __dialogue(self):
+        msg = ""
+        while msg != "kill" and msg != "disconnect" and msg != "reset":
+            msg = input("client: ")
+            self.__sock.send(msg.encode())
+            msg = self.__sock.recv(1024).decode()
+            print(msg)
         self.__sock.close()
+
+
+    def run(self):
+        if (self.__connect() ==0):
+            self.__dialogue()
+
+if __name__=="__main__":
+    if len(sys.argv) < 3:
+        client = Client("127.0.0.1",15001)
+    else :
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+        # création de l'objet client qui est aussi un thread
+        client = Client(host,port)
+    #démarrage de la thread client
+    client.start()
+    client.join()
--- servers.py
+++ servers.py
@@ -2,23 +2,33 @@ import socket
 
 def serveur():
     msg = ""
+    conn = None
+    server_socket = None
     while msg != "kill" :
+        msg = ""
         server_socket = socket.socket()
-        server_socket.bind(("localhost", 10000))
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(("localhost", 15001))
+
         server_socket.listen(1)
-        print('Serveur en attente de connexion')
+        print('Server waiting for connection')
         while msg != "kill" and msg != "reset":
-            conn, addr = server_socket.accept()
-            print (addr)
-            while msg != "kill"  and msg != "reset" and msg != "disconect":
-                data = conn.recv(1024).decode()
-                print ("Received from client: ", data)
-                msg = input('Enter a message to send: ')
-                conn.send(msg.encode())
-        conn.close()
+            msg = ""
+            try :
+                conn, addr = server_socket.accept()
+                print (addr)
+            except ConnectionError:
+                print ("connection error")
+                break
+            else :
+                while msg != "kill" and msg != "reset" and msg != "disconnect":
+                    msg = conn.recv(1024).decode()
+                    print ("Received from client: ", msg)
+                    conn.send(msg.encode())
+                conn.close()
         print ("Connection closed")
-    server_socket.close()
-    print ("Server closed")
+        server_socket.close()
+        print ("Server closed")
 
 # Coder les commande ici
 
