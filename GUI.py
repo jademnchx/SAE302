d2615a93e99ab270b045c879c8c5df3a7cbe4df8
jademnchx <jade.mnchx@gmail.com>
2022-12-07 16:28:51 +0100

    jm

--- GUI.py
+++ GUI.py
@@ -1,61 +1,73 @@
 
 from PyQt5.QtWidgets import *
 import sys
-from servers import cmd
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
+
         # Initialisation
         widget = QWidget()
         self.setCentralWidget(widget)
-        
-        grid = QGridLayout()
-        widget.setLayout(grid)
-        
         self.setWindowTitle("Test")
         self.resize(400, 300)
-        
+        grid = QGridLayout()
+        widget.setLayout(grid)
+        self.textEdit = QTextEdit()
+        self.textEdit.setEnabled(False)
+
         # Blocks
-        add = QPushButton('Add')
-        valid = QPushButton('OK')
-        cmd = QComboBox()
+        block1 = QGroupBox("Commande")
+        cmd1 = QComboBox()
         cmd2 = QComboBox()
-        
+        cmd3 = QComboBox()
+
         # Constructeur
-        self.__cmd = cmd
-        self.__valid = valid
+        self.__cmd1 = cmd1
         self.__cmd2 = cmd2
-        self.__add = add
-        
-        # ComboBox
-        cmd.addItem("disconnect")
-        cmd.addItem("connect info")
-        cmd.addItem("kill")
-        cmd.addItem("reset")
-        
+        self.__cmd3 = cmd3
+
+         # ComboBox
+        cmd1.addItem("disconnect")
+        cmd1.addItem("connect info")
+        cmd1.addItem("kill")
+        cmd1.addItem("reset")
+
         cmd2.addItem("OS")
         cmd2.addItem("CPU")
         cmd2.addItem("Name")
         cmd2.addItem("RAM")
-        
+
+        cmd3.addItem("DOS:dir")
+        cmd3.addItem("DOS:mkdir toto")
+        cmd3.addItem("Linux:ls -la")
+        cmd3.addItem("Powershell:get-process")
+        cmd3.addItem("python --version")
+        cmd3.addItem("ping 192.157.65.78")       
+
         # Placement blocks
-        grid.addWidget(self.__cmd, 0, 0)
-        grid.addWidget(self.__valid, 0, 1)
+        grid.addWidget(self.__cmd1, 0, 0)
         grid.addWidget(self.__cmd2, 2, 0)
-        grid.addWidget(self.__add, 2, 1)
+        grid.addWidget(self.__cmd3, 4, 0)
+        grid.addWidget(self.textEdit, 5, 0)
         
         # Actions blocks
-        add.clicked.connect(self.__actionadd)
-        valid.clicked.connect(self.__actionvalid)
+        self.__cmd1.currentIndexChanged.connect(self.cmd1_Clicked)
+        self.__cmd2.currentIndexChanged.connect(self.cmd2_Clicked)
+        self.__cmd3.currentIndexChanged.connect(self.cmd3_Clicked)
         
-    def __actionadd(self):
-        print("Add")
+        
+    def cmd1_Clicked(self):
+        t = self.__cmd1.currentText()
+        self.textEdit.append(f"cmd1 : {t}")
     
-    def __actionvalid(self):
-        cmd()
-        print("Valid")
+    def cmd2_Clicked(self):
+        t = self.__cmd2.currentText()
+        self.textEdit.append(f"cmd2 : {t}")
+        
+    def cmd3_Clicked(self):
+        t = self.__cmd2.currentText()
+        self.textEdit.append(f"cmd3 : {t}")
         
         
 if __name__ == '__main__':
--- servers.py
+++ servers.py
@@ -1,43 +1,50 @@
 import socket, subprocess, os
-
-def serveur():
-    msg = ""
-    conn = None
-    server_socket = None
-    while msg != "kill" :
+class Server:
+        
+    def __init__(self):
+        pass
+        
+    def serveur(self):
         msg = ""
-        server_socket = socket.socket()
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind(("localhost", 15001))
-        server_socket.listen(1)
-        print('Server waiting for connection')
-        while msg != "kill" and msg != "reset":
+        conn = None
+        server_socket = None
+        while msg != "kill" :
             msg = ""
-            try :
-                conn, addr = server_socket.accept()
-                print (addr)
-            except ConnectionError:
-                print ("connection error")
-                break
-            else :
-                while msg != "kill" and msg != "reset" and msg != "disconnect":
-                    msg = conn.recv(1024).decode()
-                    print ("Received from client: ", msg)
-                    conn.send(msg.encode())
-            conn.close()
-            print ("Connection closed")
+            server_socket = socket.socket()
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.bind(("localhost", 15001))
+            server_socket.listen(1)
+            print('Server waiting for connection')
+            while msg != "kill" and msg != "reset":
+                msg = ""
+                try :
+                    conn, addr = server_socket.accept()
+                    print (addr)
+                except ConnectionError:
+                    print ("connection error")
+                    break
+                else :
+                    while msg != "kill" and msg != "reset" and msg != "disconnect":
+                        msg = conn.recv(1024).decode()
+                        print ("Received from client: ", msg)
+                        rep = self.cmds(msg)
+                        conn.send(rep.encode())
+                    conn.close()
+        print ("Connection closed")
         server_socket.close()
         print ("Server closed")
 
-# Coder les commande ici
+    # Coder les commande ici
 
-def cmd(self, cmd):
-    msg = ""
-    if msg == "IP":
-        cmd = "ipconfig"
-        p = subprocess.Popen(cmd,stdout = subprocess.PIPE, stderr=subprocess.PIPE, encoding='cp850', shell=True)
-        print (f"résultat commande : \n {p.stdout.read()}")
-        print (f"erreur commande : \n {p.stderr.read()}")
+    def cmds(self, msg):
+        if msg == "ipconfig" or msg == "hostname" :
+            p = subprocess.Popen(msg, stdout = subprocess.PIPE, stderr=subprocess.STDOUT, encoding='cp850', shell=True)
+            rep = p.stdout.read()
+            print("rep=",rep)
+            return rep
+        else :
+            return "Commande inconnue"
 
 if __name__ == '__main__':
-    serveur()
+    serv = Server()
+    serv.serveur()
